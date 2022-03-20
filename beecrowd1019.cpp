69aa21d0309352af3e380a9a9c24787f88092a25
Joao Pedro Soares <jpsoares111@hotmail.com>
2022-03-19 21:34:46 -0300

    1019 done :)

--- beecrowd1019.cpp
+++ beecrowd1019.cpp
@@ -2,10 +2,33 @@
 
 int main(void)
 {
-	int	imputseconds;
+	int		imputseconds;
+	int	seconds;
+	int	minutes;
+	int	hours;
 
-	scanf("%i", imputseconds);
-	
-
+	hours = 0;
+	minutes = 0;
+	seconds = 0;
+	scanf("%i", &imputseconds);
+	while (imputseconds != 0)
+	{
+	if (imputseconds >= 3600)
+	{
+		imputseconds -= 3600;
+		hours++;
+	}
+	if (imputseconds < 3600 && imputseconds >= 60)
+	{
+		imputseconds -=60;
+		minutes++;
+	}
+	if (imputseconds < 60)
+	{
+	imputseconds--;
+	seconds++;
+	}
+	}
+printf("%i:%i:%i\n", hours, minutes, seconds);
 	return (0);
 }
